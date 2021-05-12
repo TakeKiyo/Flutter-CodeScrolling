52cdf238ec17e8a00d014b16985c7bd674d7b059
matsumo-and <yh134.toisanda@gmail.com>
2021-05-13 04:07:13 +0900

    BPM計測時のボタンを分かりやすくした
    アルゴリズムも多少変更した。

--- lib/models/scroll_model.dart
+++ lib/models/scroll_model.dart
@@ -3,13 +3,21 @@ import 'package:provider/provider.dart';
 
 class ScrollModel extends ChangeNotifier {
   int _tempoCount = 60;
+  get tempoCount => _tempoCount;
+
   bool _isPlaying = false;
   get isPlaying => _isPlaying;
-  get tempoCount => _tempoCount;
+
   bool _muteStatus = false;
   get muteStatus => _muteStatus;
+
   DateTime _bpmTapStartTime;
   int _bpmTapCount = 0;
+  var _bpmCalculateList = <int>[];
+  String _bpmTapText = "TAPで計測開始";
+  get bpmTapCount => _bpmTapCount;
+  get bpmTapText => _bpmTapText;
+
 
   void increment() {
     if (_tempoCount < 300) {
@@ -50,23 +58,37 @@ class ScrollModel extends ChangeNotifier {
 
     if (_bpmTapCount == 0) {
       _bpmTapStartTime = DateTime.now();
-      _bpmTapCount = 1;
+      _bpmTapCount ++;
+      _bpmTapText = "BPM計測中...";
+      notifyListeners();
     }
-    else{
+    else if (_bpmTapCount % 5 != 0){
       var _bpmDetectNow = DateTime.now();
       var _bpmDetectDiff = _bpmDetectNow.difference(_bpmTapStartTime).inMilliseconds;
+      _bpmCalculateList.add(_bpmDetectDiff);
       _bpmTapStartTime = _bpmDetectNow;
-      int _detectedBpm =  (60000 / _bpmDetectDiff).floor();
-      _tempoCount = _detectedBpm;
+      _bpmTapCount ++;
+      if (_bpmTapCount == 5) {
+        _bpmTapText = "計測終了";
+        notifyListeners();
+      }
+    }
+    else{
+      int _bpmCalculateAve = _bpmCalculateList.reduce(
+          (_bpmDiffValue, _bpmDiffElement) => _bpmDiffValue + _bpmDiffElement) ~/ _bpmCalculateList.length;
+      _tempoCount =  (60000 / _bpmCalculateAve).floor();
       if (_tempoCount < 30){_tempoCount = 30;}
       if (_tempoCount > 300){_tempoCount = 300;}
-      print("$_bpmDetectDiff");
+      print("$_bpmCalculateList");
+      resetBpmTapCount();
       notifyListeners();
     }
   }
 
   void resetBpmTapCount(){
     _bpmTapCount = 0;
+    _bpmCalculateList = <int>[];
+    _bpmTapText = "TAPで計測開始";
   }
 
 }
