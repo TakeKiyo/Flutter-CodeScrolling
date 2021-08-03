de5496146816959eba32de9eeab919cb38d6a3d6
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-03 21:04:49 +0900

    ボタンを押したら編集できるようにした ただし編集で更新されるデータはまだダミー

--- lib/pages/detail_page/detail_edit_page.dart
+++ lib/pages/detail_page/detail_edit_page.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../models/metronome_model.dart';
 import 'detail_bottom_bar.dart';
+import 'detail_page.dart';
 
 class DetailEditPage extends StatefulWidget {
   @override
@@ -17,22 +19,38 @@ class DetailEditPage extends StatefulWidget {
 }
 
 class _DetailEditForm extends State<DetailEditPage> {
-  Widget getTextWidgets(List<String> strings) {
+  List<List<String>> codeListState;
+
+  Widget getCodeListWidgets(List<String> strings, int listIndex) {
     List<Widget> list = [];
     for (var i = 0; i < strings.length; i++) {
       list.add(Flexible(
           child: TextField(
         textAlign: TextAlign.center,
-        controller: TextEditingController(text: strings[i]), //ここに初期値
+        controller: TextEditingController(text: strings[i]),
+        onChanged: (text) {
+          codeListState[listIndex][i] = text;
+        },
       )));
       list.add(Text("|"));
     }
     return new Row(children: list);
   }
 
+  void editCodeList(String docId) async {
+    FirebaseFirestore.instance.collection("Songs").doc(docId).update({
+      "codeList": ["C,F,G A,C", "A,C,D,E"],
+    });
+    Navigator.of(context).pop(
+      MaterialPageRoute(builder: (context) {
+        return DetailPage();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<List<String>> codeListState = [];
+    codeListState = [];
     List<String> splittedCodeList = widget.codeList.split("¥");
     splittedCodeList = splittedCodeList.sublist(0, splittedCodeList.length - 1);
     for (int i = 0; i < splittedCodeList.length; i++) {
@@ -62,7 +80,15 @@ class _DetailEditForm extends State<DetailEditPage> {
             children: <Widget>[
               Text("コードの編集"),
               for (int idx = 0; idx < codeListState.length; idx++)
-                getTextWidgets(codeListState[idx])
+                getCodeListWidgets(codeListState[idx], idx),
+              RaisedButton(
+                child: const Text('編集を終了'),
+                color: Colors.orange,
+                textColor: Colors.white,
+                onPressed: () {
+                  editCodeList(widget.docId);
+                },
+              ),
             ],
           ),
         ),
