c624c17a31b039b63f45853155433ab04608524d
matsumo-and <yh134.toisanda@gmail.com>
2021-08-11 15:26:56 +0900

    CountinDialogのConsumerのScopeを狭めた

--- lib/pages/detail_page/countin_dialog.dart
+++ lib/pages/detail_page/countin_dialog.dart
@@ -7,31 +7,29 @@ import 'metronome_container.dart';
 class CountInDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<MetronomeModel>(builder: (_, model, __) {
-      return Dialog(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(16))),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width / 2,
-          height: 100,
-          child: Center(
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.all(10),
-              scrollDirection: Axis.horizontal,
-              children: List.generate(model.countInTimes, (cNum) => cNum)
-                  .map((cNum) => Container(
-                        padding: EdgeInsets.all(5),
-                        child: MetronomeContainerWidget(
-                            contentState: cNum, contentNum: model.countInTimes),
-                      ))
-                  .toList(),
-            ),
-          ),
-        ),
-      );
-    });
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16))),
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width / 2,
+        height: 100,
+        child: Center(child: Consumer<MetronomeModel>(builder: (_, model, __) {
+          return ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.all(10),
+            scrollDirection: Axis.horizontal,
+            children: List.generate(model.countInTimes, (cNum) => cNum)
+                .map((cNum) => Container(
+                      padding: EdgeInsets.all(5),
+                      child: MetronomeContainerWidget(
+                          contentState: cNum, contentNum: model.countInTimes),
+                    ))
+                .toList(),
+          );
+        })),
+      ),
+    );
   }
 }
