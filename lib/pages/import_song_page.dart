7ecde16d0e8ea6627051c57d1e7816d03c94d8a0
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-17 23:20:46 +0900

    とりあえずカメラの起動とかまでできた

--- lib/pages/import_song_page.dart
+++ lib/pages/import_song_page.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_udid/flutter_udid.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ImportSong extends StatelessWidget {
   @override
@@ -29,6 +32,19 @@ class _ImportSongFormState extends State<ImportSongForm> {
     setState(() {
       copiedID = inputText;
     });
+  }
+
+  Barcode result;
+  QRViewController controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    }
+    controller.resumeCamera();
   }
 
   void importButtonClicked() {
@@ -67,6 +83,47 @@ class _ImportSongFormState extends State<ImportSongForm> {
     }
   }
 
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 200.0
+        : 300.0;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    print('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
   void importSong() async {
     String udid = await FlutterUdid.udid;
     await FirebaseFirestore.instance
@@ -101,34 +158,46 @@ class _ImportSongFormState extends State<ImportSongForm> {
     });
   }
 
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(50),
-      child: Column(
-        children: <Widget>[
-          Text(
-            "コピーしたIDを\nペーストしてください",
-            style: TextStyle(
-              color: Colors.blueAccent,
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          TextField(
-            maxLengthEnforcement: MaxLengthEnforcement.enforced,
-            style: TextStyle(color: Colors.black),
-            maxLines: 1,
-            onChanged: _handleCopiedID,
-          ),
-          ElevatedButton(
-            child: const Text('曲をインポート', style: TextStyle(color: Colors.white)),
-            style: ElevatedButton.styleFrom(primary: Colors.orange),
-            onPressed: () {
-              importButtonClicked();
-            },
-          ),
-        ],
-      ),
-    );
+        padding: const EdgeInsets.all(50),
+        child: Column(children: <Widget>[
+          Expanded(child: _buildQrView(context)),
+          Container(
+              padding: const EdgeInsets.all(30),
+              child: Column(children: <Widget>[
+                Text(
+                  "コピーしたIDをペーストしても\n追加することができます",
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextField(
+                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  enabled: true,
+                  style: TextStyle(color: Colors.red),
+                  obscureText: false,
+                  maxLines: 1,
+                  //パスワード
+                  onChanged: _handleCopiedID,
+                ),
+                ElevatedButton(
+                  child: const Text('曲をインポート',
+                      style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(primary: Colors.orange),
+                  onPressed: () {
+                    importButtonClicked();
+                  },
+                ),
+              ])),
+        ]));
   }
 }
