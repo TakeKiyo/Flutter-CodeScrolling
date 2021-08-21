12bf7dcf7c84321f2cac41b0932e42571246c0f0
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-21 16:35:53 +0900

    歌詞を表示するページを作った

---
+++ lib/pages/detail_page/scroll_lyrics_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/models/editing_song.dart';
+import 'package:my_app/models/metronome_model.dart';
+import 'package:provider/provider.dart';
+
+import 'detail_edit_page.dart';
+
+class ScrollLyricsPage extends StatefulWidget {
+  ScrollLyricsPage(this.codeList, this.bpm, this.title, this.docId,
+      this.separationList, this.rhythmList, this.lyricsList);
+  final List<String> codeList;
+  final int bpm;
+  final String title;
+  final String docId;
+  final List<String> separationList;
+  final List<String> rhythmList;
+  final List<String> lyricsList;
+
+  @override
+  _ScrollLyricsPageState createState() => _ScrollLyricsPageState();
+}
+
+class _ScrollLyricsPageState extends State<ScrollLyricsPage> {
+  // コントローラ
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // dispose the controller
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<List<String>> codeListState = [];
+    for (int i = 0; i < widget.codeList.length; i++) {
+      List<String> oneLineCode = widget.codeList[i].split(",");
+      List<String> tmp = [];
+      for (int j = 0; j < oneLineCode.length; j++) {
+        tmp.add(oneLineCode[j]);
+      }
+      codeListState.add(tmp);
+    }
+
+    List<Widget> displayedWidget() {
+      List<Widget> displayedList = [];
+      displayedList.add(TextButton(
+          onPressed: () {
+            Provider.of<MetronomeModel>(context, listen: false).tempoCount =
+                widget.bpm;
+            Provider.of<EditingSongModel>(context, listen: false).codeList =
+                widget.codeList;
+            Provider.of<EditingSongModel>(context, listen: false)
+                .separationList = widget.separationList;
+            Provider.of<EditingSongModel>(context, listen: false).rhythmList =
+                widget.rhythmList;
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return DetailEditPage(
+                    bpm: widget.bpm,
+                    title: widget.title,
+                    docId: widget.docId,
+                  );
+                },
+              ),
+            );
+          },
+          child: Text("曲を編集する")));
+      displayedList.add(TextButton(
+          onPressed: () {
+            _scrollController.animateTo(
+              _scrollController.position.maxScrollExtent,
+              curve: Curves.easeOut,
+              duration: const Duration(milliseconds: 10000),
+            );
+          },
+          child: Text("歌詞をスクロール")));
+
+      for (int listIndex = 0;
+          listIndex < widget.lyricsList.length;
+          listIndex++) {
+        if (widget.separationList.length != 0) {
+          if (listIndex == 0) {
+            displayedList.add(Text(widget.separationList[listIndex],
+                style: TextStyle(
+                  color: Colors.white,
+                  backgroundColor: Colors.black,
+                )));
+          } else {
+            if (widget.separationList[listIndex] !=
+                widget.separationList[listIndex - 1]) {
+              displayedList.add(Text(widget.separationList[listIndex],
+                  style: TextStyle(
+                    color: Colors.white,
+                    backgroundColor: Colors.black,
+                  )));
+            }
+          }
+          displayedList.add(Text(widget.lyricsList[listIndex],
+              style: TextStyle(
+                fontSize: 20,
+              )));
+        }
+      }
+
+      displayedList.add(TextButton(
+          onPressed: () {
+            _scrollController.jumpTo(
+              0.0,
+            );
+          },
+          child: Text("上まで戻る")));
+
+      return displayedList;
+    }
+
+    if (widget.lyricsList.length == 0) {
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextButton(
+              onPressed: () {
+                Provider.of<MetronomeModel>(context, listen: false).tempoCount =
+                    widget.bpm;
+                Provider.of<EditingSongModel>(context, listen: false).codeList =
+                    [];
+                Provider.of<EditingSongModel>(context, listen: false)
+                    .rhythmList = [];
+                Provider.of<EditingSongModel>(context, listen: false)
+                    .separationList = [];
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return DetailEditPage(
+                        bpm: widget.bpm,
+                        title: widget.title,
+                        docId: widget.docId,
+                      );
+                    },
+                  ),
+                );
+              },
+              child: Text("曲を編集する")),
+          Text("まだ歌詞は追加されていません")
+        ],
+      ));
+    } else {
+      return Container(
+          child: Scrollbar(
+              // controller: _scrollController,
+              isAlwaysShown: false,
+              thickness: 8.0,
+              hoverThickness: 12.0,
+              child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: ListView(
+                    padding: EdgeInsets.all(36.0),
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: displayedWidget(),
+                  ))));
+    }
+  }
+}
