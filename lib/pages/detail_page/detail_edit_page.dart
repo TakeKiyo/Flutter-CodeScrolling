b0eb3cd941a10bf88d16992693f552e127be6e3d
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-22 16:13:56 +0900

    fixed typo

--- lib/pages/detail_page/detail_edit_page.dart
+++ lib/pages/detail_page/detail_edit_page.dart
@@ -382,8 +382,8 @@ class DetailEditPage extends StatelessWidget {
                                                   },
                                                   items: <String>[
                                                     "4/4",
-                                                    "4/3",
-                                                    "4/2",
+                                                    "3/4",
+                                                    "2/4",
                                                     "6/8",
                                                   ].map<
                                                           DropdownMenuItem<
