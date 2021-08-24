72801b2008236b8b8e4b56feb08e74c1c12095b6
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-24 23:31:29 +0900

    FABみたいなボタンを追加した それでスクロールできるところまで

--- lib/pages/detail_page/lyrics_page.dart
+++ lib/pages/detail_page/lyrics_page.dart
@@ -1,56 +1,251 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:my_app/pages/detail_page/scroll_lyrics_page.dart';
+import 'package:my_app/models/editing_song.dart';
+import 'package:my_app/models/metronome_model.dart';
+import 'package:provider/provider.dart';
 
-class LyricsPage extends StatelessWidget {
+import 'detail_edit_page.dart';
+
+class LyricsPage extends StatefulWidget {
+  LyricsPage({this.bpm, this.title, this.artist, this.songKey, this.docId});
   final int bpm;
   final String title;
   final String docId;
   final String artist;
   final String songKey;
 
-  LyricsPage(
-      {Key key, this.bpm, this.title, this.artist, this.songKey, this.docId})
-      : super(key: key);
+  @override
+  _ScrollLyricsPageState createState() => _ScrollLyricsPageState();
+}
+
+class _ScrollLyricsPageState extends State<LyricsPage> {
+  final _isScrolling = false;
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose(); // dispose the controller
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('Songs')
-                .doc(docId)
-                .snapshots(),
-            builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: Text("Loading"));
-              }
-              var songDocument = snapshot.data;
-              var codeList = songDocument["codeList"].cast<String>();
-              // separationがあるか判定
-              Map<String, dynamic> dataMap =
-                  songDocument.data() as Map<String, dynamic>;
-              List<String> separation;
-              List<String> rhythmList;
-              List<String> lyricsList;
-              if (dataMap.containsKey('separation')) {
-                separation = songDocument["separation"].cast<String>();
-              } else {
-                separation = [];
-              }
-              if (dataMap.containsKey('rhythmList')) {
-                rhythmList = songDocument["rhythmList"].cast<String>();
-              } else {
-                rhythmList = [];
-              }
-              if (dataMap.containsKey("lyricsList")) {
-                lyricsList = songDocument["lyricsList"].cast<String>();
-              } else {
-                lyricsList = [];
-              }
-              return ScrollLyricsPage(codeList, bpm, title, docId, separation,
-                  rhythmList, lyricsList);
-            }));
+    return Stack(children: <Widget>[
+      Container(
+          child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Songs')
+                  .doc(widget.docId)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: Text("Loading"));
+                }
+                var songDocument = snapshot.data;
+                var codeList = songDocument["codeList"].cast<String>();
+                // separationがあるか判定
+                Map<String, dynamic> dataMap =
+                    songDocument.data() as Map<String, dynamic>;
+                List<String> separation;
+                List<String> rhythmList;
+                List<String> lyricsList;
+                if (dataMap.containsKey('separation')) {
+                  separation = songDocument["separation"].cast<String>();
+                } else {
+                  separation = [];
+                }
+                if (dataMap.containsKey('rhythmList')) {
+                  rhythmList = songDocument["rhythmList"].cast<String>();
+                } else {
+                  rhythmList = [];
+                }
+                if (dataMap.containsKey("lyricsList")) {
+                  lyricsList = songDocument["lyricsList"].cast<String>();
+                } else {
+                  lyricsList = [];
+                }
+                if (lyricsList.length == 0) {
+                  return Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextButton(
+                          onPressed: () {
+                            Provider.of<MetronomeModel>(context, listen: false)
+                                .tempoCount = widget.bpm;
+                            Provider.of<EditingSongModel>(context,
+                                    listen: false)
+                                .codeList = [];
+                            Provider.of<EditingSongModel>(context,
+                                    listen: false)
+                                .rhythmList = [];
+                            Provider.of<EditingSongModel>(context,
+                                    listen: false)
+                                .separationList = [];
+                            Provider.of<EditingSongModel>(context,
+                                    listen: false)
+                                .lyricsList = lyricsList;
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return DetailEditPage(
+                                    bpm: widget.bpm,
+                                    title: widget.title,
+                                    docId: widget.docId,
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                          child: Text("曲を編集する")),
+                      Text("まだ歌詞は追加されていません")
+                    ],
+                  ));
+                } else {
+                  List<List<String>> codeListState = [];
+                  for (int i = 0; i < codeList.length; i++) {
+                    List<String> oneLineCode = codeList[i].split(",");
+                    List<String> tmp = [];
+                    for (int j = 0; j < oneLineCode.length; j++) {
+                      tmp.add(oneLineCode[j]);
+                    }
+                    codeListState.add(tmp);
+                  }
+
+                  List<Widget> displayedWidget() {
+                    List<Widget> displayedList = [];
+                    displayedList.add(TextButton(
+                        onPressed: () {
+                          Provider.of<MetronomeModel>(context, listen: false)
+                              .tempoCount = widget.bpm;
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .codeList = codeList;
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .separationList = separation;
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .rhythmList = rhythmList;
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .lyricsList = lyricsList;
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .setDisplayType("lyrics");
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return DetailEditPage(
+                                  bpm: widget.bpm,
+                                  title: widget.title,
+                                  docId: widget.docId,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        child: Text("曲を編集する")));
+
+                    bool noLyrics = true;
+                    for (int listIndex = 0;
+                        listIndex < lyricsList.length;
+                        listIndex++) {
+                      if (lyricsList[listIndex] != "") {
+                        noLyrics = false;
+                      }
+                    }
+                    if (noLyrics) {
+                      displayedList.add(Center(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                            Text('歌詞は追加されていません。'),
+                          ])));
+                      return displayedList;
+                    }
+                    displayedList.add(TextButton(
+                        onPressed: () {
+                          _scrollController.animateTo(
+                            _scrollController.position.maxScrollExtent,
+                            curve: Curves.easeOut,
+                            duration: const Duration(milliseconds: 10000),
+                          );
+                        },
+                        child: Text("歌詞をスクロール")));
+
+                    for (int listIndex = 0;
+                        listIndex < lyricsList.length;
+                        listIndex++) {
+                      if (separation.length != 0) {
+                        if (listIndex == 0) {
+                          displayedList.add(Text(separation[listIndex],
+                              style: TextStyle(
+                                color: Colors.white,
+                                backgroundColor: Colors.black,
+                              )));
+                        } else {
+                          if (separation[listIndex] !=
+                              separation[listIndex - 1]) {
+                            displayedList.add(Text(separation[listIndex],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  backgroundColor: Colors.black,
+                                )));
+                          }
+                        }
+                        displayedList.add(Text(lyricsList[listIndex],
+                            style: TextStyle(
+                              fontSize: 20,
+                            )));
+                      }
+                    }
+                    displayedList.add(TextButton(
+                        onPressed: () {
+                          _scrollController.jumpTo(
+                            0.0,
+                          );
+                        },
+                        child: Text("上まで戻る")));
+
+                    return displayedList;
+                  }
+
+                  return Container(
+                      child: Scrollbar(
+                          isAlwaysShown: false,
+                          thickness: 8.0,
+                          hoverThickness: 12.0,
+                          child: SingleChildScrollView(
+                              controller: _scrollController,
+                              child: ListView(
+                                padding: EdgeInsets.all(36.0),
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                children: displayedWidget(),
+                              ))));
+                }
+              })),
+      Positioned(
+          bottom: 20.0,
+          right: 20.0,
+          width: 70.0,
+          height: 70.0,
+          child: FloatingActionButton(
+              backgroundColor: Colors.deepOrange[800],
+              child: Icon(Icons.arrow_downward),
+              onPressed: () {
+                _scrollController.animateTo(
+                  _scrollController.position.maxScrollExtent,
+                  curve: Curves.easeOut,
+                  duration: Duration(
+                      milliseconds:
+                          Provider.of<EditingSongModel>(context, listen: false)
+                              .scrollSpeed),
+                );
+              }))
+    ]);
   }
 }
