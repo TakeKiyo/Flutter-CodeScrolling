7cb043f0138c15ac9c9ac828a55b143040c5f5a4
TakeKiyo <d8sk4ueum@gmail.com>
2021-08-29 15:46:38 +0900

    編集するときに、memberIDに入ってないと編集できないようにした

--- lib/pages/detail_page/detail_edit_page.dart
+++ lib/pages/detail_page/detail_edit_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_udid/flutter_udid.dart';
 import 'package:my_app/models/editing_song.dart';
 import 'package:provider/provider.dart';
 
@@ -209,6 +210,7 @@ class DetailEditPage extends StatelessWidget {
     }
 
     void submitCodeList(String docId) async {
+      String udid = await FlutterUdid.udid;
       FirebaseFirestore.instance.collection("Songs").doc(docId).update({
         "codeList": formatCodeList(
             Provider.of<EditingSongModel>(context, listen: false).codeList),
@@ -219,6 +221,8 @@ class DetailEditPage extends StatelessWidget {
         "lyricsList":
             Provider.of<EditingSongModel>(context, listen: false).lyricsList,
         "updatedAt": DateTime.now(),
+        "udid": udid,
+        "type": "edit",
       });
       Navigator.of(context).pop(
         MaterialPageRoute(builder: (context) {
