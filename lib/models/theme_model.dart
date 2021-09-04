613dd5b1555e47665cfb4953968948add97c8027
matsumo-and <yh134.toisanda@gmail.com>
2021-09-04 16:20:27 +0900

    primarySwatch変更した

--- lib/models/theme_model.dart
+++ lib/models/theme_model.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ThemeModel extends ChangeNotifier {
-  ThemeData _currentTheme = ThemeData(
-    appBarTheme: AppBarTheme(color: Colors.white),
-    primaryColor: Colors.white,
+  MaterialColor white = const MaterialColor(
+    0xFFFFFFFF,
+    const <int, Color>{
+      50: const Color(0xFFFFFFFF),
+      100: const Color(0xFFFFFFFF),
+      200: const Color(0xFFFFFFFF),
+      300: const Color(0xFFFFFFFF),
+      400: const Color(0xFFFFFFFF),
+      500: const Color(0xFFFFFFFF),
+      600: const Color(0xFFFFFFFF),
+      700: const Color(0xFFFFFFFF),
+      800: const Color(0xFFFFFFFF),
+      900: const Color(0xFFFFFFFF),
+    },
   );
+
+  ThemeData _currentTheme = ThemeData(primarySwatch: Colors.white);
   get currentTheme => _currentTheme;
 
   //  themeIndex = 0:自動, 1:ライトモード, 2:ダークモード
@@ -14,18 +27,12 @@ class ThemeModel extends ChangeNotifier {
     switch (fetchedIndex) {
       case 0:
         _themeIndex = fetchedIndex;
-        _currentTheme = ThemeData(
-          appBarTheme: AppBarTheme(color: Colors.white),
-          primaryColor: Colors.white,
-        );
+        _currentTheme = ThemeData(primarySwatch: Colors.white);
         break;
 
       case 1:
         _themeIndex = fetchedIndex;
-        _currentTheme = ThemeData(
-          appBarTheme: AppBarTheme(color: Colors.white),
-          primaryColor: Colors.white,
-        );
+        _currentTheme = ThemeData(primarySwatch: Colors.white);
         break;
 
       case 2:
