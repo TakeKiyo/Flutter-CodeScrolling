b5ecd759f2a339954f74d46aa32195d7fad1db95
matsumo-and <yh134.toisanda@gmail.com>
2021-09-05 05:45:10 +0900

    captionを追加

--- lib/models/theme_model.dart
+++ lib/models/theme_model.dart
@@ -9,38 +9,41 @@ class ThemeModel extends ChangeNotifier {
   }
 
   final ThemeData _lightTheme = ThemeData(
-      primarySwatch: Colors.grey,
-      primaryColorLight: Colors.blue[500],
-      primaryColor: Colors.blue[800],
-      primaryColorDark: Colors.blue[900],
-      canvasColor: Colors.grey[200],
-      colorScheme: ColorScheme(
-          primary: Colors.blue[800],
-          primaryVariant: Colors.blue[900],
-          secondary: Colors.blue[800],
-          secondaryVariant: Colors.blue[900],
-          surface: Colors.white,
-          background: Colors.white,
-          error: Colors.red[600],
-          onPrimary: Colors.white,
-          onSecondary: Colors.white,
-          onSurface: Colors.black,
-          onBackground: Colors.black,
-          onError: Colors.white,
-          brightness: Brightness.light),
-      appBarTheme: AppBarTheme(
-          color: Colors.grey[200],
-          textTheme: TextTheme(
-              headline6: TextStyle(color: Colors.black, fontSize: 20.0)),
-          actionsIconTheme: IconThemeData(color: Colors.black),
-          iconTheme: IconThemeData(color: Colors.black)),
-      toggleableActiveColor: Colors.blue[800],
-      primaryTextTheme: TextTheme(caption: TextStyle()));
+    primaryColorLight: Colors.blue[500],
+    primaryColor: Colors.blue[800],
+    primaryColorDark: Colors.blue[900],
+    canvasColor: Colors.grey[200],
+    colorScheme: ColorScheme(
+        //primary => TextSelection, TextButton, Sliderなど広範に利用される
+        //secondary => accentColor(Floating Action Button)など。
+        primary: Colors.blue[800],
+        primaryVariant: Colors.blue[900],
+        secondary: Colors.blue[800],
+        secondaryVariant: Colors.blue[900],
+        surface: Colors.white,
+        background: Colors.white,
+        error: Colors.red[600],
+        onPrimary: Colors.white,
+        onSecondary: Colors.white,
+        onSurface: Colors.black,
+        onBackground: Colors.black,
+        onError: Colors.white,
+        brightness: Brightness.light),
+    appBarTheme: AppBarTheme(
+        //default color = primaryColor
+        color: Colors.grey[200],
+        textTheme: const TextTheme(
+            //headline6 == title
+            headline6: TextStyle(color: Colors.black, fontSize: 20.0)),
+        actionsIconTheme: const IconThemeData(color: Colors.black),
+        iconTheme: const IconThemeData(color: Colors.black)),
+    //チェックボックスの色をprimaryColorに合わせる。default = primarySwatch
+    toggleableActiveColor: Colors.blue[800],
+  );
 
   //_lightTheme.copyWith() method doesn't work
   final ThemeData _darkTheme = ThemeData(
     brightness: Brightness.dark,
-    primarySwatch: Colors.grey,
     primaryColorLight: Colors.blue[500],
     primaryColor: Colors.blue[800],
     primaryColorDark: Colors.blue[900],
@@ -61,17 +64,17 @@ class ThemeModel extends ChangeNotifier {
     appBarTheme: AppBarTheme(
         color: Colors.grey[850],
         brightness: Brightness.dark,
-        textTheme: TextTheme(
-            headline6: TextStyle(color: Colors.white, fontSize: 20.0)),
-        actionsIconTheme: IconThemeData(color: Colors.white),
-        iconTheme: IconThemeData(color: Colors.white)),
+        textTheme: const TextTheme(
+            headline6: const TextStyle(color: Colors.white, fontSize: 20.0)),
+        actionsIconTheme: const IconThemeData(color: Colors.white),
+        iconTheme: const IconThemeData(color: Colors.white)),
     toggleableActiveColor: Colors.blue[800],
+    //TextFieldのunFocus/Focus時の色を指定。
     inputDecorationTheme: InputDecorationTheme(
-      enabledBorder:
-          UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey[200])),
-      focusedBorder:
-          UnderlineInputBorder(borderSide: BorderSide(color: Colors.blue[800])),
-    ),
+        enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[200])),
+        focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue[800]))),
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: Colors.blue[800],
       selectionColor: Colors.blue[800],
