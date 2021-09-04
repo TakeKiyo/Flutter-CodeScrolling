318d32ac507ac36f985f285f0bcac8dffa84dbe2
matsumo-and <yh134.toisanda@gmail.com>
2021-09-04 16:29:15 +0900

    whiteをグローバルに定義した

--- lib/models/theme_model.dart
+++ lib/models/theme_model.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
-class ThemeModel extends ChangeNotifier {
-  MaterialColor white = const MaterialColor(
-    0xFFFFFFFF,
-    const <int, Color>{
-      50: const Color(0xFFFFFFFF),
-      100: const Color(0xFFFFFFFF),
-      200: const Color(0xFFFFFFFF),
-      300: const Color(0xFFFFFFFF),
-      400: const Color(0xFFFFFFFF),
-      500: const Color(0xFFFFFFFF),
-      600: const Color(0xFFFFFFFF),
-      700: const Color(0xFFFFFFFF),
-      800: const Color(0xFFFFFFFF),
-      900: const Color(0xFFFFFFFF),
-    },
-  );
+MaterialColor white = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0xFFFFFFFF),
+    100: const Color(0xFFFFFFFF),
+    200: const Color(0xFFFFFFFF),
+    300: const Color(0xFFFFFFFF),
+    400: const Color(0xFFFFFFFF),
+    500: const Color(0xFFFFFFFF),
+    600: const Color(0xFFFFFFFF),
+    700: const Color(0xFFFFFFFF),
+    800: const Color(0xFFFFFFFF),
+    900: const Color(0xFFFFFFFF),
+  },
+);
 
-  ThemeData _currentTheme = ThemeData(primarySwatch: Colors.white);
+class ThemeModel extends ChangeNotifier {
+  ThemeData _currentTheme = ThemeData(primarySwatch: white);
   get currentTheme => _currentTheme;
 
   //  themeIndex = 0:自動, 1:ライトモード, 2:ダークモード
