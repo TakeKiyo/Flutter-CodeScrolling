40277a4d74d81b9094b815b2ecaa61d32f8e63ec
TakeKiyo <d8sk4ueum@gmail.com>
2021-09-06 14:19:54 +0900

    デザイン整えた

--- lib/pages/login_form.dart
+++ lib/pages/login_form.dart
@@ -31,112 +31,141 @@ class _LoginFormState extends State<LoginForm> {
   Widget build(BuildContext context) {
     return Consumer<AuthModel>(builder: (_, model, __) {
       return Scaffold(
-        appBar: AppBar(
-          title: Text('ログイン'),
-        ),
-        body: Form(
-            key: _formKey,
-            child: Container(
-              padding: const EdgeInsets.only(left: 30, right: 30, top: 10.0),
-              child: Column(children: <Widget>[
-                TextFormField(
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                  ),
-                  decoration: const InputDecoration(
-                    labelText: 'メールアドレス',
-                  ),
-                  onChanged: (String value) {
-                    setEmail(value);
-                  },
-                  // ignore: missing_return
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'メールアドレスを入力してください。';
-                    }
-                  },
-                ),
-                TextFormField(
-                  obscureText: !_showPassword,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'ログイン',
+            ),
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () {
+                  Navigator.of(context).popUntil((route) => route.isFirst);
+                },
+              ),
+            ),
+            actions: [],
+          ),
+          body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                Text(
+                  'Bandout',
                   style: TextStyle(
-                    fontSize: 20.0,
+                    fontFamily: 'Rochester',
+                    fontSize: 50,
                   ),
-                  decoration: InputDecoration(
-                    labelText: 'パスワード',
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _showPassword = !_showPassword;
-                        });
-                      },
-                      icon: const Icon(Icons.remove_red_eye),
-                    ),
-                  ),
-                  onChanged: (String value) {
-                    setPassword(value);
-                  },
-                  // ignore: missing_return
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'パスワードを入力してください';
-                    }
-                  },
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                  child: OutlinedButton(
-                      child:
-                          Text("ログイン", style: const TextStyle(fontSize: 25.0)),
-                      style: OutlinedButton.styleFrom(
-                        primary: Theme.of(context).textTheme.headline6.color,
-                        side: const BorderSide(),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState.validate()) {
-                          // FocusScope.of(context).unfocus();
-                          try {
-                            if (await model.login(_email, _password)) {
-                              await Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) {
-                                  return SongsList();
-                                }),
-                              );
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  backgroundColor:
-                                      Theme.of(context).colorScheme.error,
-                                  content:
-                                      const Text('エラーが発生しました。\n もう一度お試しください。'),
-                                  duration: const Duration(seconds: 2),
-                                  action: SnackBarAction(
-                                    label: 'OK',
-                                    onPressed: () {},
-                                  ),
-                                ),
-                              );
+                Form(
+                    key: _formKey,
+                    child: Container(
+                      padding:
+                          const EdgeInsets.only(left: 30, right: 30, top: 10.0),
+                      child: Column(children: <Widget>[
+                        TextFormField(
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                          ),
+                          decoration: const InputDecoration(
+                            labelText: 'メールアドレス',
+                          ),
+                          onChanged: (String value) {
+                            setEmail(value);
+                          },
+                          // ignore: missing_return
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'メールアドレスを入力してください。';
                             }
-                          } catch (e) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.error,
-                                content:
-                                    const Text('エラーが発生しました。\n もう一度お試しください。'),
-                                duration: const Duration(seconds: 2),
-                                action: SnackBarAction(
-                                  label: 'OK',
-                                  onPressed: () {},
-                                ),
+                          },
+                        ),
+                        TextFormField(
+                          obscureText: !_showPassword,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                          ),
+                          decoration: InputDecoration(
+                            labelText: 'パスワード',
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _showPassword = !_showPassword;
+                                });
+                              },
+                              icon: const Icon(Icons.remove_red_eye),
+                            ),
+                          ),
+                          onChanged: (String value) {
+                            setPassword(value);
+                          },
+                          // ignore: missing_return
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'パスワードを入力してください';
+                            }
+                          },
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(top: 20.0, bottom: 20.0),
+                          child: OutlinedButton(
+                              child: Text("ログイン",
+                                  style: const TextStyle(fontSize: 25.0)),
+                              style: OutlinedButton.styleFrom(
+                                primary:
+                                    Theme.of(context).textTheme.headline6.color,
+                                side: const BorderSide(),
                               ),
-                            );
-                          }
-                        }
-                      }),
-                ),
-              ]),
-            )),
-      );
+                              onPressed: () async {
+                                if (_formKey.currentState.validate()) {
+                                  // FocusScope.of(context).unfocus();
+                                  try {
+                                    if (await model.login(_email, _password)) {
+                                      await Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) {
+                                          return SongsList();
+                                        }),
+                                      );
+                                    } else {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          backgroundColor: Theme.of(context)
+                                              .colorScheme
+                                              .error,
+                                          content: const Text(
+                                              'エラーが発生しました。\n もう一度お試しください。'),
+                                          duration: const Duration(seconds: 2),
+                                          action: SnackBarAction(
+                                            label: 'OK',
+                                            onPressed: () {},
+                                          ),
+                                        ),
+                                      );
+                                    }
+                                  } catch (e) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        backgroundColor:
+                                            Theme.of(context).colorScheme.error,
+                                        content: const Text(
+                                            'エラーが発生しました。\n もう一度お試しください。'),
+                                        duration: const Duration(seconds: 2),
+                                        action: SnackBarAction(
+                                          label: 'OK',
+                                          onPressed: () {},
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                }
+                              }),
+                        ),
+                      ]),
+                    )),
+              ])));
     });
   }
 }
--- lib/pages/signin_form.dart
+++ lib/pages/signin_form.dart
@@ -32,125 +32,145 @@ class _SigninFormState extends State<SigninForm> {
   Widget build(BuildContext context) {
     return Consumer<AuthModel>(builder: (_, model, __) {
       return Scaffold(
-        appBar: AppBar(
-          title: Text('新規登録'),
-        ),
-        body: Form(
-            key: _formKey,
-            child: Container(
-              padding: EdgeInsets.only(left: 30, right: 30, top: 10.0),
-              child: Column(children: <Widget>[
-                TextFormField(
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                  decoration: InputDecoration(
-                    labelText: 'メールアドレス',
-                  ),
-                  onChanged: (String value) {
-                    setEmail(value);
-                  },
-                  // ignore: missing_return
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'メールアドレスを入力してください。';
-                    }
-                  },
-                ),
-                TextFormField(
-                  obscureText: !_showPassword,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                  decoration: InputDecoration(
-                    labelText: 'パスワード',
-                    suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _showPassword = !_showPassword;
-                        });
+          body: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+            Text(
+              'Bandout',
+              style: TextStyle(
+                fontFamily: 'Rochester',
+                fontSize: 50,
+              ),
+            ),
+            Form(
+                key: _formKey,
+                child: Container(
+                  padding: EdgeInsets.only(left: 30, right: 30, top: 10.0),
+                  child: Column(children: <Widget>[
+                    TextFormField(
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'メールアドレス',
+                      ),
+                      onChanged: (String value) {
+                        setEmail(value);
                       },
-                      icon: const Icon(Icons.remove_red_eye),
-                    ),
-                  ),
-                  onChanged: (String value) {
-                    setPassword(value);
-                  },
-                  // ignore: missing_return
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'パスワードを入力してください';
-                    }
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
-                  child: OutlinedButton(
-                    child: Text("新規登録", style: const TextStyle(fontSize: 25.0)),
-                    style: OutlinedButton.styleFrom(
-                      primary: Theme.of(context).textTheme.headline6.color,
-                      side: const BorderSide(),
-                    ),
-                    onPressed: () async {
-                      if (_formKey.currentState.validate()) {
-                        try {
-                          if (await model.signin(_email, _password)) {
-                            await Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context) {
-                                return SongsList();
-                              }),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                backgroundColor:
-                                    Theme.of(context).colorScheme.error,
-                                content:
-                                    const Text('エラーが発生しました。\n もう一度お試しください。'),
-                                duration: const Duration(seconds: 2),
-                                action: SnackBarAction(
-                                  label: 'OK',
-                                  onPressed: () {},
-                                ),
-                              ),
-                            );
-                          }
-                        } catch (e) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.error,
-                              content: const Text('エラーが発生しました。\n もう一度お試しください。'),
-                              duration: const Duration(seconds: 2),
-                              action: SnackBarAction(
-                                label: 'OK',
-                                onPressed: () {},
-                              ),
-                            ),
-                          );
+                      // ignore: missing_return
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'メールアドレスを入力してください。';
                         }
-                      }
-                    },
-                  ),
-                ),
-                OutlinedButton(
-                  child:
-                      Text("ログインはこちら", style: const TextStyle(fontSize: 25.0)),
-                  style: OutlinedButton.styleFrom(
-                    primary: Theme.of(context).textTheme.headline6.color,
-                    side: const BorderSide(),
-                  ),
-                  onPressed: () async {
-                    FocusScope.of(context).unfocus();
-                    await Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return LoginForm();
-                    }));
-                  },
-                ),
-              ]),
-            )),
-      );
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: !_showPassword,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'パスワード',
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _showPassword = !_showPassword;
+                            });
+                          },
+                          icon: const Icon(Icons.remove_red_eye),
+                        ),
+                      ),
+                      onChanged: (String value) {
+                        setPassword(value);
+                      },
+                      // ignore: missing_return
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'パスワードを入力してください';
+                        }
+                      },
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: OutlinedButton(
+                            child: Text("登録",
+                                style: const TextStyle(fontSize: 25.0)),
+                            style: OutlinedButton.styleFrom(
+                              primary:
+                                  Theme.of(context).textTheme.headline6.color,
+                              side: const BorderSide(),
+                            ),
+                            onPressed: () async {
+                              if (_formKey.currentState.validate()) {
+                                try {
+                                  if (await model.signin(_email, _password)) {
+                                    await Navigator.of(context).push(
+                                      MaterialPageRoute(builder: (context) {
+                                        return SongsList();
+                                      }),
+                                    );
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                        backgroundColor:
+                                            Theme.of(context).colorScheme.error,
+                                        content: const Text(
+                                            'エラーが発生しました。\n もう一度お試しください。'),
+                                        duration: const Duration(seconds: 2),
+                                        action: SnackBarAction(
+                                          label: 'OK',
+                                          onPressed: () {},
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                } catch (e) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      backgroundColor:
+                                          Theme.of(context).colorScheme.error,
+                                      content: const Text(
+                                          'エラーが発生しました。\n もう一度お試しください。'),
+                                      duration: const Duration(seconds: 2),
+                                      action: SnackBarAction(
+                                        label: 'OK',
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                  );
+                                }
+                              }
+                            },
+                          ),
+                        )),
+                    Text(
+                      "アカウントをお持ちですか？",
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.caption.color,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    OutlinedButton(
+                      child:
+                          Text("ログイン", style: const TextStyle(fontSize: 20.0)),
+                      style: OutlinedButton.styleFrom(
+                        primary: Theme.of(context).textTheme.headline6.color,
+                        side: const BorderSide(),
+                      ),
+                      onPressed: () async {
+                        FocusScope.of(context).unfocus();
+                        await Navigator.of(context)
+                            .push(MaterialPageRoute(builder: (context) {
+                          return LoginForm();
+                        }));
+                      },
+                    ),
+                  ]),
+                )),
+          ])));
     });
   }
 }
