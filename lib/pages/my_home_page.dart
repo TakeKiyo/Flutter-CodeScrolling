f61c7e58c2a9bf04751dd97a001d684aa12488b5
TakeKiyo <d8sk4ueum@gmail.com>
2021-09-06 11:15:30 +0900

    最初に新規登録画面を表示するようにした

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/models/auth_model.dart';
+import 'package:my_app/pages/signin_form.dart';
 import 'package:my_app/pages/songs_list.dart';
 import 'package:provider/provider.dart';
-
-import 'login_form.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bool _loggedIn = context.watch<AuthModel>().loggedIn;
-    return _loggedIn ? SongsList() : LoginForm();
+    return _loggedIn ? SongsList() : SigninForm();
   }
 }
