51c56f47fd1b841d05898ca887abb7a7091b9acf
TakeKiyo <d8sk4ueum@gmail.com>
2021-09-13 22:46:47 +0900

    縦画面固定した

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
   await Firebase.initializeApp();
   runApp(MyApp());
 }
